f12936c7391e9c602bdabeb506569dafcd3e167e
JhonatanLucena <jhonatan.jhon9@hotmail.com>
2023-03-20 18:34:41 -0400

    Altereção no pc funfando

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/ProjetoWeb2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="/ProjetoWeb2/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    
+    <link href="/ProjetoWeb2/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
+    <script src="/ProjetoWeb2/js/bootstrap.bundle.min.js"  crossorigin="anonymous"></script>
+   
     <title>Pagina Inicial</title>
 </head>
 <body>
